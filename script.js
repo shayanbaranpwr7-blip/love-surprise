7c10be55b39d5aaca5a89c0e346cb8825427057f
shayanbaranpwr7-blip <shayanbaranpwr7@gmail.com>
2026-07-31 16:34:02 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const canvas = document.getElementById("rain");
+const ctx = canvas.getContext("2d");
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let drops = [];
+
+for(let i=0;i<250;i++){
+    drops.push({
+        x:Math.random()*canvas.width,
+        y:Math.random()*canvas.height,
+        l:Math.random()*20+10,
+        s:Math.random()*6+4
+    });
+}
+
+function rain(){
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    ctx.strokeStyle="rgba(255,255,255,.25)";
+    ctx.lineWidth=1;
+
+    drops.forEach(d=>{
+
+        ctx.beginPath();
+
+        ctx.moveTo(d.x,d.y);
+
+        ctx.lineTo(d.x+
