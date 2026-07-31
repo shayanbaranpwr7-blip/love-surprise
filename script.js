8a0486b9650fce5441034831ae9e1da6c367ebb2
shayanbaranpwr7-blip <shayanbaranpwr7@gmail.com>
2026-07-31 21:21:25 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// ===== Three.js Scene =====
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+75,
+window.innerWidth/window.innerHeight,
+0.1,
+2000
+);
+
+const renderer = new THREE.WebGLRenderer({
+alpha:true,
+antialias:true
+});
+
+renderer.setSize(
+window.innerWidth,
+window.innerHeight
+);
+
+renderer.setPixelRatio(window.devicePixelRatio);
+
+renderer.domElement.style.position="fixed";
+renderer.domElement.style.top="0";
+renderer.domElement.style.left="0";
+renderer.domElement.style.zIndex="-1";
+
+document.body.appendChild(renderer.domElement);
 const canvas = document.getElementById("rain");
 const ctx = canvas.getContext("2d");
 
